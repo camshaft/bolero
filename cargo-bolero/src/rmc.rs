a13243a12bcb63384a8b4d1a0f4d6162f124e3c5
Zyad Hassan <zyadh@amazon.com>
2021-11-12 18:26:49 -0800

    Addressed PR comments

--- cargo-bolero/src/rmc.rs
+++ cargo-bolero/src/rmc.rs
@@ -1,27 +1,19 @@
 use crate::{exec, test, Selection};
-use anyhow::{anyhow, Result};
-use bit_set::BitSet;
-use core::cmp::Ordering;
-use std::{
-    fs,
-    io::{BufRead, BufReader, Result as IOResult, Write},
-    path::{Path, PathBuf},
-    process::Command,
-};
+use anyhow::Result;
+use std::process::Command;
 
 pub(crate) fn test(selection: &Selection, test_args: &test::Args) -> Result<()> {
     let _ = selection;
     let _ = test_args;
-    let mut rmc_cmd = String::from("cargo rmc");
-    rmc_cmd.push_str(" --function ");
-    rmc_cmd.push_str(&selection.test());
-    rmc_cmd.push_str(" --cbmc-args --object-bits 16");
-    Command::new("sh")
-          .env("RUSTFLAGS", "--cfg=fuzzing_rmc --cfg=fuzzing")
-          .arg("-c")
-          .arg(rmc_cmd)
-          .spawn()
-          .expect("failed to execute process");
+    let mut cmd = Command::new("cargo");
+    cmd.arg("rmc")
+        .arg("--function")
+        .arg(selection.test())
+        .arg("--cbmc-args")
+        .arg("--object-bits")
+        .arg("16");
+
+    exec(cmd)?;
 
     Ok(())
 }
