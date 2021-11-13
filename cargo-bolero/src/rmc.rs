e4d4377acf0810c9084856dde387318cce03b043
Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>
2021-11-12 19:16:04 -0800

    Added --engine rmc (#65)

---
+++ cargo-bolero/src/rmc.rs
@@ -0,0 +1,19 @@
+use crate::{exec, test, Selection};
+use anyhow::Result;
+use std::process::Command;
+
+pub(crate) fn test(selection: &Selection, test_args: &test::Args) -> Result<()> {
+    let _ = selection;
+    let _ = test_args;
+    let mut cmd = Command::new("cargo");
+    cmd.arg("rmc")
+        .arg("--function")
+        .arg(selection.test())
+        .arg("--cbmc-args")
+        .arg("--object-bits")
+        .arg("16");
+
+    exec(cmd)?;
+
+    Ok(())
+}
